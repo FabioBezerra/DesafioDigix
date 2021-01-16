b8bb65b2744881acede9b361c28bd0dafdfd0352
Fabio Bezerra de Souza <fabio.bezerra@db1.com.br>
2021-01-16 18:21:39 -0400

    Criado projeto para testes de unidade

---
+++ src/Desafio.Common/Desafio.Domain.Tests/FamiliaDomain/Services/VerificadorDeIdadeDoPretendenteTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio.Domain.Tests.FamiliaDomain.Services
+{
+    public class VerificadorDeIdadeDoPretendenteTests
+    {
+    }
+}
